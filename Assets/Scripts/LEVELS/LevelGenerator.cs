71aaa5d5697f13086090c44afb352e660be8c172
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2022-07-28 00:08:47 -0700

    LEVEL ISNT AS STUPID ANYMORE (did checks and balances for the procedural generation) Still buggy

--- Assets/Scripts/LEVELS/LevelGenerator.cs
+++ Assets/Scripts/LEVELS/LevelGenerator.cs
@@ -18,10 +18,39 @@ public class LevelGenerator : MonoBehaviour
     {
         
     }
+    public List<GameObject> SpawnCorrectRoomDamnIt(RoomSpawner.direction direction)
+    {
+        List<GameObject> newList = new List<GameObject>();
+        foreach(GameObject room in FloorTemplates)
+        {
+            for (int i = 0; i < room.transform.GetChild(1).transform.childCount; i++)
+            {
+                
+                if (direction == RoomSpawner.direction.Down && room.transform.GetChild(1).transform.GetChild(i).GetComponent<RoomSpawner>().WhatDirection == RoomSpawner.direction.Up)
+                {
+                    newList.Add(room);
+                }
+                if (direction == RoomSpawner.direction.Up && room.transform.GetChild(1).transform.GetChild(i).GetComponent<RoomSpawner>().WhatDirection == RoomSpawner.direction.Down)
+                {
+                    newList.Add(room);
+                }
+                if (direction == RoomSpawner.direction.Right && room.transform.GetChild(1).transform.GetChild(i).GetComponent<RoomSpawner>().WhatDirection == RoomSpawner.direction.Left)
+                {
+                    newList.Add(room);
+                }
+                if (direction == RoomSpawner.direction.Left && room.transform.GetChild(1).transform.GetChild(i).GetComponent<RoomSpawner>().WhatDirection == RoomSpawner.direction.Right)
+                {
+                    newList.Add(room);
+                }
+            }
+        }
+        return newList;
+    }
     public static GameObject SpawnRoom(RoomSpawner.direction direction, Vector3 PrevRoomPos)
     {
         var LM = new LevelGenerator();
         LM = GameObject.Find("GameManager").GetComponent<LevelGenerator>();
+        List<GameObject> newFloorTemplates = LM.SpawnCorrectRoomDamnIt(direction);
         Vector3 spawnLocation = PrevRoomPos;
         if (direction == RoomSpawner.direction.Up)
         {
@@ -39,7 +68,7 @@ public class LevelGenerator : MonoBehaviour
         {
             spawnLocation = new Vector3(PrevRoomPos.x + 18f, PrevRoomPos.y);
         }
-        GameObject spawnedRoom = Instantiate(LM.FloorTemplates[Random.Range(0, LM.FloorTemplates.Length)], spawnLocation, new Quaternion(0,0,0,0), GameObject.Find("MAP").transform);
+        GameObject spawnedRoom = Instantiate(newFloorTemplates[Random.Range(0, newFloorTemplates.Count)], spawnLocation, new Quaternion(0,0,0,0), GameObject.Find("MAP").transform);
         var attributes = spawnedRoom.GetComponent<SpawnRoomItems>();
         attributes.DestroyUselessDoor(direction);
         return spawnedRoom;
