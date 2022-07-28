2413940baaf15078a72240355cadc684826db799
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2022-07-28 00:57:57 -0700

    Cleaned up level generator and added floors

--- Assets/Scripts/LEVELS/LevelGenerator.cs
+++ Assets/Scripts/LEVELS/LevelGenerator.cs
@@ -54,19 +54,19 @@ public class LevelGenerator : MonoBehaviour
         Vector3 spawnLocation = PrevRoomPos;
         if (direction == RoomSpawner.direction.Up)
         {
-            spawnLocation = new Vector3(PrevRoomPos.x, PrevRoomPos.y + 24f);
+            spawnLocation = new Vector3(PrevRoomPos.x, PrevRoomPos.y + 23.18f);
         }
         if (direction == RoomSpawner.direction.Down)
         {
-            spawnLocation = new Vector3(PrevRoomPos.x, PrevRoomPos.y + -24f);
+            spawnLocation = new Vector3(PrevRoomPos.x, PrevRoomPos.y + -23.18f);
         }
         if (direction == RoomSpawner.direction.Left)
         {
-            spawnLocation = new Vector3(PrevRoomPos.x + -18, PrevRoomPos.y);
+            spawnLocation = new Vector3(PrevRoomPos.x + -17.6f, PrevRoomPos.y);
         }
         if (direction == RoomSpawner.direction.Right)
         {
-            spawnLocation = new Vector3(PrevRoomPos.x + 18f, PrevRoomPos.y);
+            spawnLocation = new Vector3(PrevRoomPos.x + 17.6f, PrevRoomPos.y);
         }
         GameObject spawnedRoom = Instantiate(newFloorTemplates[Random.Range(0, newFloorTemplates.Count)], spawnLocation, new Quaternion(0,0,0,0), GameObject.Find("MAP").transform);
         var attributes = spawnedRoom.GetComponent<SpawnRoomItems>();
